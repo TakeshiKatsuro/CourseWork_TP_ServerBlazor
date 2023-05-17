5ec2107476a333e2682e56d20049dc400e640295
Russell <russofat@gmail.com>
2023-05-17 19:51:58 +0300

    Добавьте файлы проекта.

---
+++ CourseWork_TP_ServerBlazor/Pages/EmployeeListBase.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseWork_TP_ServerBlazor.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> MyProperty { get; set; }
+    }
+}
+
---
+++ CourseWork_TP_ServerBlazor/Startup.cs
@@ -0,0 +1,62 @@
+using CourseWork_TP_ServerBlazor.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseWork_TP_ServerBlazor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Этот метод вызывается средой выполнения. Используйте этот метод для добавления служб в контейнер.
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor(); //этот метод добавляет серверные службы Blazor
+            services.AddSingleton<WeatherForecastService>();
+        }
+        // Этот метод вызывается средой выполнения. Используйте этот метод для настройки конвейера HTTP-запросов.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // Значение HSTS по умолчанию — 30 дней.Вы можете изменить это для производственных сценариев
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub(); //настраивает конечную точку сигнала(SignalR) на сервере
+                endpoints.MapFallbackToPage("/_Host"); //все остальные запросы сопоставляются с этим хостом страницы 
+            });
+        }
+    }
+}
